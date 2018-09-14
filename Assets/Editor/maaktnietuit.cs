da871d66402721c4fc6ad9e2b419fde12dd7c2e2
NoelWriter <schr0112@hz.nl>
2018-09-14 12:06:43 +0200

    aa

---
+++ Assets/Editor/maaktnietuit.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    [MenuItem("Build/Build iOS")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "/Build";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
